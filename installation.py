7475192651b4739dd8c9fb167f8ad5d760af44b0
telegramcores <69624248+telegramcores@users.noreply.github.com>
2022-01-26 13:00:53 +0300

    Server по умолчанию

--- installation.py
+++ installation.py
@@ -10,15 +10,11 @@ def emerge_base():
 
     call_cmd_and_print_cmd('emerge app-portage/gentoolkit')
 
-    print(call_cmd_and_print_cmd('eselect profile list'))
-
-    print('select profile:')
-
-    profile_choice = sp.check_output('read -t 20 CHOICE; [ -z $CHOICE ] && echo 6 || echo $CHOICE', shell=True).strip().decode()
-
-    call_cmd_and_print_cmd(f'eselect profile set --force {profile_choice}')
-
-    print(call_cmd_and_print_cmd('eselect profile list'))
+    #print(call_cmd_and_print_cmd('eselect profile list'))
+    #print('select profile:')
+    #profile_choice = sp.check_output('read -t 20 CHOICE; [ -z $CHOICE ] && echo 6 || echo $CHOICE', shell=True).strip().decode()
+    #call_cmd_and_print_cmd(f'eselect profile set --force {profile_choice}')
+    #print(call_cmd_and_print_cmd('eselect profile list'))
 
     call_cmd_and_print_cmd('emerge app-portage/cpuid2cpuflags')
     call_cmd_and_print_cmd('echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags')
